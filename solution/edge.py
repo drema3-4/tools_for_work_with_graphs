6bc84e467daa697065016a72736f6e4a0cc02276
danko382 <daniil-kondrashov-rabotaet@yandex.ru>
2024-08-31 22:34:10 +0300

    мелкие изменения

--- solution/edge.py
+++ solution/edge.py
@@ -1,11 +1,19 @@
 class Edge:
+    '''Класс ребра графа.
+    У каждого ребра есть два конца - две вершины,
+    считается, (в случае ориентированного графа) что ребро направлено
+    от vertex_1 к vertex_2.
+    Так же, в случае взвешенного графа, у ребра есть вес.
+    Два ребра считаются одинаковыми, если их vertex_1 и vertex_2 совпадают.'''
 
     def __init__(self, vertex_1: str, vertex_2: str, weight: int = None):
+        '''Конструктор'''
         self.vertex_1 = vertex_1
         self.vertex_2 = vertex_2
         self.weight = weight
 
     def __str__(self) -> str:
+        '''Строковое представление ребра.'''
         return self.vertex_1 + \
                '-' + \
                (('(' + str(self.weight) + ')') if self.weight != None else '') + \
@@ -13,10 +21,13 @@ class Edge:
                self.vertex_2
     
     def __eq__(self, other) -> bool:
+        '''Сравнение двух рёбер.
+        Два ребра считаются одинаковыми, если их vertex_1 и vertex_2 совпадают.'''
         return self.vertex_1 == other.get_vertex_1() and \
                self.vertex_2 == other.get_vertex_2()
 
     def __hash__(self) -> int:
+        '''Хэш код ребра.'''
         hash = ''
 
         for i in range(len(self.vertex_1)):
@@ -28,19 +39,25 @@ class Edge:
         return eval(hash)
 
     def set_vertex_1(self, vertex_1: str) -> None:
+        '''Сеттер вершины 1.'''
         self.vertex_1 = vertex_1
 
     def set_vertex_2(self, vertex_2: str) -> None:
+        '''Сеттер вершины 2.'''
         self.vertex_2 = vertex_2
 
     def set_weight(self, weight: int) -> None:
+        '''Сеттер веса ребра.'''
         self.weight = weight
 
     def get_vertex_1(self) -> str:
+        '''Геттер вершины 1.'''
         return self.vertex_1
     
     def get_vertex_2(self) -> str:
+        '''Геттер вершины 2.'''
         return self.vertex_2
     
     def get_weight(self) -> int:
+        '''Геттер веса ребра.'''
         return self.weight
--- solution/vertex.py
+++ solution/vertex.py
@@ -1,34 +1,27 @@
 import edge.py
 
 class Vertex:
+    '''Данный класс отвечает за вершину графа.
+    У вершины есть имя (строка).
+    Две вершины считаются одинаковыми, если их имена совпадают.'''
 
     def __init__(self, name: str) -> None:
+        '''Конструктор'''
         self.name = name
 
-    def __init__(self, name: str, edges: set[Edge]) -> None:
-        self.name = name
-        self.edges = edges
-
-    def __eq__(self, other) -> bool:
-        return self.name == other.get_name()
-
-    def __hash__(self) -> int:
-        return ord(self.name)
-    
     def __str__(self) -> str:
-        return self.name
-
-    def set_name(self, name: str) -> None:
-        self.name = name
-
-    def add_edge(self, edge: Edge) -> None:
-        self.edges.add(edge)
-
-    def del_edge(self, name: str) -> None:
-        
-
-    def get_name(self) -> str:
+        '''Строковое представление вершины.'''
         return self.name
     
-    def get_edges(self) -> set[Edge]:
-        return self.edges
+    def __hash__(self) -> int:
+        '''Хэш вершины'''
+        return int(''.join([str(ord(symb)) for symb in self.name]))
+    
+    def __eq__(self, other) -> bool:
+        '''Сравнение вершин.
+        Две вершины считаются одинаковыми, если их имена равны.'''
+        return self.name == other.get_name()
+    
+    def get_name(self) -> str:
+        '''Возвращает имя вершины'''
+        return self.name
